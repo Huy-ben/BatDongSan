20a1c92fe05e871fbbadef5bfd759f5281520d6f
Huy Ben <benpham06@gmail.com>
2026-03-24 12:35:00 +0700

    [Huy Ben] model category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\client\PageController::class, 'index']);
+
+Route::get('/tests', [App\Http\Controllers\client\TestController::class, 'index']);
